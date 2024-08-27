1671048f7b73bbcb07c54d5ab40a5f4897b6fb7a
Juanes <valdezjuanes014@gmail.com>
2024-08-26 21:55:01 -0500

    Added site responsive and auth basic with static credential

--- src/app/feature/auth/pages/login/login.component.ts
+++ src/app/feature/auth/pages/login/login.component.ts
@@ -1,23 +1,46 @@
 import { Component } from '@angular/core';
 import {Router, RouterLink} from "@angular/router";
+import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [
-    RouterLink
+    RouterLink,
+    FormsModule,
+    NgIf,
+    ReactiveFormsModule
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
-  constructor(private router: Router) {
+  loginForm: FormGroup;
+  private readonly validUsername: string = 'jevaldes@unicesar.edu.co';
+  private readonly validPassword: string = '12345';
+  public errorMessage: string = '';
+
+  constructor(private router: Router, private fb: FormBuilder, ) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]]
+    });
   }
 
 
   public async signIn(): Promise<void> {
-    await this.router.navigateByUrl('/dashboard/home');
+    const email = this.loginForm.get('email')?.value;
+    const password = this.loginForm.get('password')?.value;
+
+    if (email == this.validUsername && password == this.validPassword) {
+      this.errorMessage = "";
+      await this.router.navigateByUrl('/dashboard/home');
+    } else {
+      this.errorMessage = 'Credenciales incorrectas. Inténtalo de nuevo.';
+    }
+
   }
 
 }
