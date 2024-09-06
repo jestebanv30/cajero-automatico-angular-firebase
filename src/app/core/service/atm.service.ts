166446f88c3433015e9520e239969d76fdf0666c
Juanes <valdezjuanes014@gmail.com>
2024-09-06 14:00:58 -0500

    Updated method atm service

--- src/app/core/service/atm.service.ts
+++ src/app/core/service/atm.service.ts
@@ -8,9 +8,6 @@ export class AtmService {
   private cashSelect = new BehaviorSubject<number>(0);
   public getCashSelect$ = this.cashSelect.asObservable();
 
-  private readonly billetes = [100000, 50000, 20000, 10000];
-  private readonly maxBilletes = [4, 3, 2, 1];
-
   constructor() {}
 
   public setCashSelect(amount: number): void {
@@ -18,50 +15,68 @@ export class AtmService {
   }
 
   public calculateBills(monto: number) {
-    const conteoBilletes = [0, 0, 0, 0];
-    let montoRestante = monto;
+    const billetes = [10000, 20000, 50000, 100000];
+    let suma = 0;
+    const entregarBilletes = new Map<number, number>();
 
-    while (montoRestante > 0) {
-      let suma = 0;
-      const activacion = [0, 0, 0, 0];
+    if (monto % 10000 !== 0) {
+      return { exito: false, mensaje: "El monto solicitado debe ser un múltiplo de 10k" };
+    }
 
-      for (let i = this.billetes.length - 1; i >= 0; i--) {
-        const cantidadMaxima = this.maxBilletes[i];
-        const valorBillete = this.billetes[i];
+    // Inicializa el mapa con el conteo de billetes en 0
+    billetes.forEach(billete => {
+      entregarBilletes.set(billete, 0);
+    });
 
-        let cantidadBilletes = 0;
-        while (
-          cantidadBilletes < cantidadMaxima &&
-          suma + valorBillete <= montoRestante
-          ) {
-          suma += valorBillete;
-          cantidadBilletes++;
-          activacion[i]++;
+    while (monto !== suma) {
+      let progreso = false;
+
+      for (let i = 0; i < billetes.length; i++) {
+        if (suma + billetes[i] <= monto) {
+          suma += billetes[i];
+          entregarBilletes.set(billetes[i], entregarBilletes.get(billetes[i])! + 1);
+          progreso = true;
+
+          // Intentar también con billetes posteriores en la misma ronda
+          for (let j = i + 1; j < billetes.length; j++) {
+            if (suma + billetes[j] <= monto) {
+              suma += billetes[j];
+              entregarBilletes.set(billetes[j], entregarBilletes.get(billetes[j])! + 1);
+              progreso = true;
+            }
+          }
         }
       }
 
-      if (suma > 0) {
-        for (let i = 0; i < this.billetes.length; i++) {
-          conteoBilletes[i] += activacion[i];
+      // Si no se pudo avanzar, reiniciar el proceso
+      if (!progreso) {
+        // Reiniciar el proceso de conteo
+        suma = 0;
+        entregarBilletes.forEach((_, billete) => {
+          entregarBilletes.set(billete, 0);
+        });
+
+        // Comprobar si se ha hecho algún progreso en la ronda actual
+        const sumaActual = Array.from(entregarBilletes.entries())
+          .reduce((acc, [billete, cantidad]) => acc + billete * cantidad, 0);
+        if (sumaActual >= monto) {
+          break;
         }
-        montoRestante -= suma;
-      } else {
-        break;
       }
     }
 
-    if (montoRestante === 0) {
+    if (suma === monto) {
       return {
         billetes: {
-          "100k": conteoBilletes[0],
-          "50k": conteoBilletes[1],
-          "20k": conteoBilletes[2],
-          "10k": conteoBilletes[3],
+          "10k": entregarBilletes.get(10000) || 0,
+          "20k": entregarBilletes.get(20000) || 0,
+          "50k": entregarBilletes.get(50000) || 0,
+          "100k": entregarBilletes.get(100000) || 0,
         },
         exito: true,
       };
     } else {
-      return {exito: false};
+      return { exito: false, mensaje: "No es posible retirar el monto solicitado" };
     }
   }
 
